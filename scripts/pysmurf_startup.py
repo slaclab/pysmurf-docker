b83ddf759acc6b53a5b974792497f6c76b30931f
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-03-22 16:57:15 -0700

    Add numpy and matplotlib to startup python script

--- scripts/pysmurf_startup.py
+++ scripts/pysmurf_startup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env ipython3
 
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 import pysmurf
 
 # The epics prefix is defined in the environmental variable 'EPICS_PREFIX'
