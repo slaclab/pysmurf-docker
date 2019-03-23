638b917f0a5d60f395188ab7d8f4baa258c02877
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-03-22 17:12:08 -0700

    Capture the CONFIG_VAR Env var value in python

--- scripts/pysmurf_startup.py
+++ scripts/pysmurf_startup.py
@@ -7,3 +7,9 @@ import pysmurf
 
 # The epics prefix is defined in the environmental variable 'EPICS_PREFIX'
 epics_prefix = os.getenv('EPICS_PREFIX')
+
+# The path to the default configuration file, if any, is defined
+# in the environmental variable 'CONFIG_FILE'
+config_file = os.getenv('CONFIG_FILE')
+if config_file == None:
+	del config_file
